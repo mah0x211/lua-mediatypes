70a8a2922c1ceffbca76a8c76d6e73a50a581795
Masatoshi Teruya <mah0x211@gmail.com>
2018-04-21 23:16:15 +0900

    Change API specification

--- mediatypes.lua
+++ mediatypes.lua
@@ -25,37 +25,71 @@
   Created by Masatoshi Teruya on 14/07/04.
 
 --]]
--- module
-local split = require('util.string').split;
-local setmetatable = setmetatable;
--- default mime
+--- modules
+local strfind = string.find;
+local strsub = string.sub;
+local strmatch = string.match;
+local strgmatch = string.gmatch;
+--- constants
 local DEFAULT_MIME = require('mediatypes.default');
 
 
--- private funcs
-local function parseMIME( str, typMap, extMap )
-    local list, ext, mime;
+--- split
+-- @param str
+-- @param pat
+-- @return arr
+local function split( str, pat )
+    local arr = {};
+    local idx = 0;
+    local cur = 1;
+    local last = #str + 1;
+    local head, tail = strfind( str, pat, cur );
 
-    -- mime format:
+    while head do
+        if head ~= cur then
+            idx = idx + 1;
+            arr[idx] = strsub( str, cur, head - 1 );
+        end
+        cur = tail + 1;
+        head, tail = strfind( str, pat, cur );
+    end
+
+    if cur < last then
+        arr[idx + 1] = strsub( str, cur );
+    end
+
+    return arr;
+end
+
+
+--- parse
+-- @param mimetypes
+-- @param mime2exts
+-- @param ext2mime
+local function parse( mimetypes, mime2exts, ext2mime )
+    -- parse following mime format:
     --  mime/type       ext1 ext2 ext3; # comments
     -- read each line
-    for line in string.gmatch( str, '[^\n\r]+') do
-        -- not comment line
-        if not line:find('^%s*#') then
-            list = split( line, '%s+' );
-            mime = list[1];
+    for line in strgmatch( mimetypes, '[^\n\r]+' ) do
+        -- remove comment line
+        line = strmatch( line, '^[^#;]+' )
+        if line then
+            local list = split( line, '%s+' );
+            local mime = list[1];
+
             -- select extension
             for i = 2, #list do
-                ext = list[i]:match('%w+');
+                local ext = strmatch( list[i], '%w+' );
+
                 -- found new extension
-                if ext and not extMap[ext] then
-                    -- update ext-map table
-                    extMap[ext] = mime;
-                    -- update mime-map table
-                    if not typMap[mime] then
-                        typMap[mime] = { ext };
+                if ext and not ext2mime[ext] then
+                    -- update exts table
+                    ext2mime[ext] = mime;
+                    -- update types table
+                    if not mime2exts[mime] then
+                        mime2exts[mime] = { ext };
                     else
-                        typMap[mime][#typMap[mime] + 1] = ext;
+                        mime2exts[mime][#mime2exts[mime] + 1] = ext;
                     end
                 end
             end
@@ -68,37 +102,49 @@ end
 local MediaType = {};
 
 
-function MediaType:getExt( mime )
-    return self.typMap[mime];
+--- getexts
+-- @param mime
+-- @return exts
+function MediaType:getexts( mime )
+    return self.mime2exts[mime];
 end
 
 
-function MediaType:getMIME( ext )
-    return self.extMap[ext];
+--- getmime
+-- @param ext
+-- @return mime
+function MediaType:getmime( ext )
+    return self.ext2mime[ext];
 end
 
 
-function MediaType:readTypes( mimetypes )
-    if mimetypes ~= nil and type( mimetypes ) ~= 'string' then
+--- read
+-- @param mimetypes
+function MediaType:read( mimetypes )
+    if type( mimetypes ) == 'string' then
+        parse( mimetypes, self.mime2exts, self.ext2mime );
+    else
         error('mimetypes must be string');
     end
-
-    parseMIME( mimetypes or DEFAULT_MIME, self.typMap, self.extMap );
 end
 
 
+--- new
+-- @param mimetypes
+-- @return MediaType
 local function new( mimetypes )
     local self = {};
 
-    self.typMap = {};
-    self.extMap = {};
+    self.mime2exts = {};
+    self.ext2mime = {};
     setmetatable( self, {
         __index = MediaType
     });
-    self:readTypes( mimetypes );
+    self:read( mimetypes == nil and DEFAULT_MIME or mimetypes );
 
     return self;
 end
+
 
 return {
     new = new
