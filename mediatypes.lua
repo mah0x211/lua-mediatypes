caac2229910e6e9b16b31e4bcf04b7fd4b0783d5
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-01-14 11:59:45 +0900

    Code formatting with lua-formatter

--- mediatypes.lua
+++ mediatypes.lua
@@ -1,95 +1,95 @@
---[[
-
-  Copyright (C) 2014 Masatoshi Teruya
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-  mediatypes.lua
-  lua-mediatypes
-  Created by Masatoshi Teruya on 14/07/04.
-
---]]
+--
+-- Copyright (C) 2022 Masatoshi Fukunaga
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--
 --- modules
-local strfind = string.find;
-local strsub = string.sub;
-local strmatch = string.match;
-local strgmatch = string.gmatch;
+local error = error
+local type = type
+local find = string.find
+local gmatch = string.gmatch
+local match = string.match
+local sub = string.sub
+local setmetatable = setmetatable
 --- constants
-local DEFAULT_MIME = require('mediatypes.default');
-
+local DEFAULT_MIME = require('mediatypes.default')
 
 --- split
--- @param str
--- @param pat
--- @return arr
-local function split( str, pat )
-    local arr = {};
-    local idx = 0;
-    local cur = 1;
-    local last = #str + 1;
-    local head, tail = strfind( str, pat, cur );
+--- @param str string
+--- @param pat string
+--- @return string[] arr
+local function split(str, pat)
+    local arr = {}
+    local idx = 0
+    local cur = 1
+    local last = #str + 1
+    local head, tail = find(str, pat, cur)
 
     while head do
         if head ~= cur then
-            idx = idx + 1;
-            arr[idx] = strsub( str, cur, head - 1 );
+            idx = idx + 1
+            arr[idx] = sub(str, cur, head - 1)
         end
-        cur = tail + 1;
-        head, tail = strfind( str, pat, cur );
+        cur = tail + 1
+        head, tail = find(str, pat, cur)
     end
 
     if cur < last then
-        arr[idx + 1] = strsub( str, cur );
+        arr[idx + 1] = sub(str, cur)
     end
 
-    return arr;
+    return arr
 end
 
-
 --- parse
--- @param mimetypes
--- @param mime2exts
--- @param ext2mime
-local function parse( mimetypes, mime2exts, ext2mime )
-    -- parse following mime format:
-    --  mime/type       ext1 ext2 ext3; # comments
+--- @param mimetypes string
+--- @param mime2exts table
+--- @param ext2mime table
+local function parse(mimetypes, mime2exts, ext2mime)
+    --
+    -- parses the following mime format string:
+    --
+    --  mime/type       ext1 ext2 ext3 # comments
+    --
     -- read each line
-    for line in strgmatch( mimetypes, '[^\n\r]+' ) do
+    for line in gmatch(mimetypes, '[^\n\r]+') do
         -- remove comment line
-        line = strmatch( line, '^[^#;]+' )
+        line = match(line, '^[^#]+')
         if line then
-            local list = split( line, '%s+' );
-            local mime = list[1];
+            local list = split(line, '%s+')
+            local mime = list[1]
 
             -- select extension
             for i = 2, #list do
-                local ext = strmatch( list[i], '%w+' );
+                local ext = match(list[i], '%w+')
 
                 -- found new extension
                 if ext and not ext2mime[ext] then
                     -- update exts table
-                    ext2mime[ext] = mime;
+                    ext2mime[ext] = mime
                     -- update types table
                     if not mime2exts[mime] then
-                        mime2exts[mime] = { ext };
+                        mime2exts[mime] = {
+                            ext,
+                        }
                     else
-                        mime2exts[mime][#mime2exts[mime] + 1] = ext;
+                        mime2exts[mime][#mime2exts[mime] + 1] = ext
                     end
                 end
             end
@@ -97,56 +97,50 @@ local function parse( mimetypes, mime2exts, ext2mime )
     end
 end
 
-
--- class
-local MediaType = {};
-
+--- @class MediaType
+--- @field mime2exts table<string, string[]>
+--- @field ext2mime table<string, string>
+local MediaType = {}
+MediaType.__index = MediaType
 
 --- getexts
--- @param mime
--- @return exts
-function MediaType:getexts( mime )
-    return self.mime2exts[mime];
+--- @param mime string
+--- @return string[] exts
+function MediaType:getexts(mime)
+    return self.mime2exts[mime]
 end
-
 
 --- getmime
--- @param ext
--- @return mime
-function MediaType:getmime( ext )
-    return self.ext2mime[ext];
+--- @param ext string
+--- @return string mime
+function MediaType:getmime(ext)
+    return self.ext2mime[ext]
 end
-
 
 --- read
--- @param mimetypes
-function MediaType:read( mimetypes )
-    if type( mimetypes ) == 'string' then
-        parse( mimetypes, self.mime2exts, self.ext2mime );
-    else
-        error('mimetypes must be string');
+--- @param mimetypes string
+function MediaType:read(mimetypes)
+    if type(mimetypes) ~= 'string' then
+        error('mimetypes must be string')
     end
+    parse(mimetypes, self.mime2exts, self.ext2mime)
 end
-
 
 --- new
--- @param mimetypes
--- @return MediaType
-local function new( mimetypes )
-    local self = {};
+--- @param mimetypes string
+--- @return MediaType
+local function new(mimetypes)
+    local self = setmetatable({
+        mime2exts = {},
+        ext2mime = {},
+    }, MediaType)
 
-    self.mime2exts = {};
-    self.ext2mime = {};
-    setmetatable( self, {
-        __index = MediaType
-    });
-    self:read( mimetypes == nil and DEFAULT_MIME or mimetypes );
+    self:read(mimetypes == nil and DEFAULT_MIME or mimetypes)
 
-    return self;
+    return self
 end
 
-
 return {
-    new = new
-};
+    new = new,
+}
 
